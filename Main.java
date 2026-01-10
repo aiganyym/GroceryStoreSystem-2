1d304e06db785aec2d1a5651ab82000db167eee7
aiganyym <253601@astanait.edu.kz>
2026-01-10 08:59:16 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,18 +1,62 @@
-import java.util.ArrayList;
+import java.util.ArrayList; 
+import java.util.Scanner;   
 
 public class Main {
     public static void main(String[] args) {
-
+        
         ArrayList<User> users = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
 
-        users.add(new Customer("Aiganym", "87001112233", 50));
-        users.add(new Employee("Batyr", "87004445566", "Cashier"));
+        while (running) {
+            System.out.println("\n--- Store Management System ---");
+            System.out.println("1. Add Customer");
+            System.out.println("2. Add Employee");
+            System.out.println("3. View All (Polymorphism)");
+            System.out.println("4. Exit");
+            System.out.print("Choose option: ");
 
-        System.out.println("--- Store Management System ---");
+            int choice = scanner.nextInt();
+            scanner.nextLine(); 
 
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter name: ");
+                    String cName = scanner.nextLine();
+                    System.out.print("Enter phone: ");
+                    String cPhone = scanner.nextLine();
+                    System.out.print("Enter loyalty points: ");
+                    int points = scanner.nextInt();
+                    
+                    users.add(new Customer(cName, cPhone, points));
+                    break;
+                case 2:
+                    System.out.print("Enter name: ");
+                    String eName = scanner.nextLine();
+                    System.out.print("Enter phone: ");
+                    String ePhone = scanner.nextLine();
+                    scanner.nextLine();
+                    System.out.print("Enter position: ");
+                    String pos = scanner.nextLine();
+                    users.add(new Employee(eName, ePhone, pos));
+                    break;
+                case 3:
+                    System.out.println("\n--- User List ---");
+                    for (User u : users) {
+                        
+                        u.displayInfo();
 
-        for (User u : users) {
-            u.displayInfo();
+                        
+                        if (u instanceof Customer) {
+                            System.out.println("   -> This user has loyalty points.");
+                        }
+                    }
+                    break;
+                case 4:
+                    running = false;
+                    break;
+            }
         }
+        System.out.println("Program finished.");
     }
 }
